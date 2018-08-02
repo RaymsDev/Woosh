e11b7cf88f4e8b669dad494843f59460a75892a1
Rémy Laffuge <remy.laffuge@neos-sdi.com>
2018-08-02 11:27:51 +0200

    Import db

---
+++ Mountain/dbo/Tables/Dwarf.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Dwarf] (
+    [Id]     INT           NOT NULL,
+    [Name]   NVARCHAR (50) NULL,
+    [IdCave] INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Dwarf_Cave] FOREIGN KEY ([IdCave]) REFERENCES [dbo].[Cave] ([Id])
+);
+
